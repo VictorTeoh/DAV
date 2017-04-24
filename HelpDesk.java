3561e6201872b6b62d8543a4657cf255cbd30a49
A11ard <apeng@stuy.edu>
2017-04-23 22:52:12 -0400

    Update HelpDesk.java

--- HelpDesk.java
+++ HelpDesk.java
@@ -5,33 +5,50 @@ public class HelpDesk{
 
     private static ArrayPriorityQueue _line;//collection of tickets to be handled  
     private static ArrayList _archive;//collection of tickets that were handled 
+    private static Ticket _currentTicket; 		
     private static int _counter; //assigns id to each subsequent ticket
     public HelpDesk(){
 	_line = new ArrayPriorityQueue(); 
-	_archive = new ArrayPriorityQueue();
+	_archive = new ArrayList();
+	_currentTicket = null;     
 	_counter = 0;
     }
 
-    public void enQueue(Ticket t){//adds a ticket to the line
-	t.setID(_counter);
-	_line.add(t);
-	_counter++;
+    public void enQueue(Ticket t){//adds a ticket to the line	
+	_line.add(t);	
     }
 
-    public Ticket deQueue(){//removes and returns ticket of highest priority
-	return _line.removeMin(); 
+    public Ticket deQueue(){//removes and returns ticket of highest priority. This ticket is now being handled by the help desk 	    
+	Ticket ret = _line.removeMin();
+	ret.setID(_counter);    
+	_currentTicket = ret;
+	_counter++;    
+	return ret; 
     }
 
     public Ticket peekNext(){//returns ticket of highest priority
 	return _line.peekMin(); 
+    }			
+	
+    public boolean resolveTicket(Ticket t){//resolves ticket by setting status at 0, meaning it is resolved
+	t.setStatus(0);     
+	return true;     
     }
-
-    public void archiveTicket(Ticket t){//put a ticket into the archive
-	_archive.add(t); 
-    }
+	
+    public boolean archiveTicket(Ticket t){//tries to archive the ticket, if the status is resolved, then it is archived and return true; false otherwise
+	if(t.getStatus() == 0){
+		System.out.println("Issue has been resolved and stored in our database. Have a good day, " + t.getName() + "."); 
+		_archive.add(t);
+		_currentTicket = null; 
+		return true; 
+	}
+	else{
+		return false; 
+	}
+    }		
 
     public Ticket getFromArchive(int ticketNumber){//get a ticket from the archive
-	return _archive.getList().get(ticketNumber); 
+	return _archive.get(ticketNumber); 
     }
 
     public static void main(String[] args){
